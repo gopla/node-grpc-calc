f342d1b9c2a76c51b216fa144b7e66fc917d9e1c
gopla <yudhistn@gmail.com>
2020-10-06 07:52:32 +0700

    chore(app): Change folder structure

---
+++ app/client/client.js
@@ -0,0 +1,18 @@
+const PROTO_PATH = './app/calc.proto'
+const grpc = require('grpc')
+const protoLoader = require('@grpc/proto-loader')
+
+let packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+	keepCase: true,
+	longs: String,
+	enums: String,
+	arrays: true,
+})
+
+const CalcService = grpc.loadPackageDefinition(packageDefinition).CalcService
+const client = new CalcService(
+	'localhost:50051',
+	grpc.credentials.createInsecure(),
+)
+
+module.exports = client
